fe7ad0ca26d7e2bb76481d2a737bdd0724555796
nvinden <nvinden@uoguelph.ca>
2021-10-12 18:02:26 -0400

    nvinden

--- train.py
+++ train.py
@@ -63,8 +63,6 @@ def train(save_name):
     model = model.to(device)
     criterion = criterion.to(device)
 
-    epoch_num = 0
-
     for epoch in range(start_epoch, TRAIN_CONFIG["n_epochs"]):
         model.train()
         model.requires_grad_()
