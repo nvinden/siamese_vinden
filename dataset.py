84f0087b227a6b9d3f0277aaf41b5d4193a7597d
nvinden <nvinden@uoguelph.ca>
2021-11-03 15:29:04 -0400

    nvinden

--- dataset.py
+++ dataset.py
@@ -559,7 +559,7 @@ class RDataset(Dataset):
                     if key not in entries_concat:
                         entries_concat[key] = val
                     else:
-                        if entries_concat[key].shape[1] == val.shape[1]:
+                        if entries_concat[key].shape[1] != val.shape[1]:
                             print("BROKEN")
                             print(entries_concat[key].shape)
                             print(val.shape)
