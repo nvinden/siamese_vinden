701bafd7fba815190c09db7b4da7d20b59c16222
nvinden <nvinden@uoguelph.ca>
2021-11-24 14:55:01 -0500

    nvinden

--- train.py
+++ train.py
@@ -72,7 +72,7 @@ def train(save_name, k):
         total_epoch_loss = 0
         total_pairs = len(ds)
 
-        data_save_condition = ((epoch % 5 == 0) or epoch == TRAIN_CONFIG["n_epochs"] - 1) and epoch != 0
+        data_save_condition = ((epoch % 5 == 0) or epoch == TRAIN_CONFIG["n_epochs"] - 1)# and epoch != 0
 
         if data_save_condition:
             model_dict = dict()
